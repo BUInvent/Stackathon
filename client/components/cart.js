f2664f7ba0735dfe0771d0606bb36dbafc0d7e9c
Cody Jensen <jensen.cody.e@gmail.com>
2019-01-09 14:23:14 -0600

    'actually added cart files'

---
+++ client/components/cart.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Container, Row, Col} from 'reactstrap'
+import CartItem from './cart-item'
+
+class Cart extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col sm="12" md={{size: 6, offset: 3}}>
+            <h1>Your Cart</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12" md={{size: 6}}>
+            <ul>
+              {this.props.cartItems.map(item => {
+                //debugger
+                return <CartItem {...item} />
+              })}
+            </ul>
+          </Col>
+          <Col sm="12" md={{size: 6}}>
+            <p>${this.props.cartSum}</p>
+            <p>$5.00</p>
+            <p>$20.00</p>
+            <p>___________</p>
+            <p>${this.props.cartSum + 5 + 20}</p>
+            <button>Checkout</button>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    cartItems: state.cart.cart,
+    cartSum: state.cart.price
+  }
+}
+
+export default connect(mapStateToProps)(Cart)
