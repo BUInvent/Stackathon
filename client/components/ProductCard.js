f5d684284422c0004f3cc2e4d188eabb40ff15d3
Shawn Michael Larkin <shawn.michael.larkin@gmail.com>
2019-01-11 18:17:23 -0500

    everything up till now

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -9,34 +9,33 @@ import {
   CardSubtitle,
   Button
 } from 'reactstrap'
-import {isAbsolute} from 'path'
 
 const ProductCard = ({product}) => {
-  const {id, imageURL, name} = product
+  const {id, imageURL, name, shortDescription} = product
   return (
-    <div className="m-2">
-      {' '}
-      <Card
-        style={{
-          width: 250,
-          height: 325
-        }}
-      >
-        <Link to={`/products/${id}`}>
-          <CardImg top width="100%" src={imageURL} alt={`picture-of-${name}`} />
-        </Link>
-        <CardBody>
-          <CardTitle>
-            <h5>{name}</h5>
-          </CardTitle>
-          <Button
+    <Card
+      style={{
+        width: 250,
+        height: 325
+      }}
+      className="m-2"
+    >
+      <Link to={`/products/${id}`}>
+        <CardImg top width="100%" src={imageURL} alt={`picture-of-${name}`} />
+      </Link>
+      <CardBody style={{position: 'relative'}}>
+        <CardTitle>
+          <strong>{name}</strong>
+        </CardTitle>
+        <CardSubtitle>{shortDescription}</CardSubtitle>
+        <Button
+          style={{position: 'absolute', bottom: 15}}
           /* onClick={THUNK: addToCart} */
-          >
-            Buy
-          </Button>
-        </CardBody>
-      </Card>
-    </div>
+        >
+          Buy
+        </Button>
+      </CardBody>
+    </Card>
   )
 }
 
