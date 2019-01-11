43b5d0c2e897952fd75c0e5506d7564c778bf299
Unknown <jmmorris2@mail.bradley.edu>
2019-01-11 17:17:47 -0600

    Clicking add adds form

--- client/components/address.js
+++ client/components/address.js
@@ -1,7 +1,27 @@
 import React from 'react'
-import {Container, Media, Row, Col, Button, ButtonGroup} from 'reactstrap'
+import {
+  Container,
+  Media,
+  Row,
+  Col,
+  Button,
+  ButtonGroup,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from 'reactstrap'
 
 class Address extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showEdit: false
+    }
+  }
+
   delete(id) {
     this.props.delete(id)
   }
@@ -40,13 +60,56 @@ class Address extends React.Component {
           <Container>
             <Row>
               <Col sm="12">
-                <Button className="my-4" color="primary">
+                <Button
+                  className="my-4"
+                  color="primary"
+                  onClick={() => {
+                    this.setState({showEdit: true})
+                  }}
+                >
                   Add new
                 </Button>
               </Col>
             </Row>
           </Container>
         </Media>
+
+        {this.state.showEdit ? (
+          <Form>
+            <FormGroup>
+              <Label for="address1">Address line 1</Label>
+              <Input type="text" name="address1" id="address1" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="address2">Address line 2</Label>
+              <Input type="textarea" name="address2" id="address2" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="zip">ZIP</Label>
+              <Input type="textarea" name="text" id="zip" />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="state">Country: </Label>
+              <Input type="select" name="select" id="state">
+                <option>IL</option>
+                <option>AZ</option>
+              </Input>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="Country">Country: </Label>
+              <Input type="select" name="select" id="Country">
+                <option>United States</option>
+                <option>Canada</option>
+              </Input>
+            </FormGroup>
+
+            <Button>Submit</Button>
+          </Form>
+        ) : null}
       </div>
     )
   }
