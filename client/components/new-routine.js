b24e1a09311fe1d68bd69a3d84d29022ec889651
Unknown <jmmorris2@mail.bradley.edu>
2019-01-18 16:07:16 -0600

    Connect routine to user

--- client/components/new-routine.js
+++ client/components/new-routine.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Button, Form, FormGroup, Input, Row, Col} from 'reactstrap'
+import {connect} from 'react-redux'
 
 /**
  * COMPONENT
@@ -7,7 +8,13 @@ import {Button, Form, FormGroup, Input, Row, Col} from 'reactstrap'
 class NewRoutine extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {exercise: [], inputExercise: '', inputSets: 3, inputReps: 8}
+    this.state = {
+      exercise: [],
+      inputExercise: '',
+      inputSets: 3,
+      inputReps: 8,
+      userId: this.props.userId
+    }
   }
 
   handleChange = event => {
@@ -45,7 +52,14 @@ class NewRoutine extends React.Component {
           <center>Create New Routine</center>
         </h1>
 
-        <Form onSubmit={this.addExercise}>
+        <Form action="/api/routines" method="post">
+          <Input
+            type="hidden"
+            id="userID"
+            name="user"
+            value={this.props.userId}
+          />
+
           <FormGroup className="col-sm-6 offset-sm-3">
             <Input
               type="text"
@@ -55,78 +69,88 @@ class NewRoutine extends React.Component {
             />
           </FormGroup>
 
-          <FormGroup className="col-sm-6 offset-sm-3">
-            <Input
-              onChange={this.handleChange}
-              type="text"
-              name="inputExercise"
-              id="inputExercise"
-              placeholder="Exercise"
-              value={this.state.inputExercise}
-            />
-          </FormGroup>
+          <Form onSubmit={this.addExercise}>
+            <FormGroup className="col-sm-6 offset-sm-3">
+              <Input
+                onChange={this.handleChange}
+                type="text"
+                name="inputExercise"
+                id="inputExercise"
+                placeholder="Exercise"
+                value={this.state.inputExercise}
+              />
+            </FormGroup>
+
+            <FormGroup className="col-sm-6 offset-sm-3">
+              <Input
+                placeholder="Sets"
+                type="number"
+                step="1"
+                value={this.state.inputSets}
+              />
+            </FormGroup>
+            <FormGroup className="col-sm-6 offset-sm-3">
+              <Input
+                placeholder="Reps"
+                type="number"
+                step="1"
+                value={this.state.inputReps}
+              />
+            </FormGroup>
+
+            <FormGroup className="col-sm-6 offset-sm-3">
+              <Button type="submit" color="primary" block>
+                Add Exercise
+              </Button>
+            </FormGroup>
+          </Form>
+
+          <ul>
+            {this.state.exercise.map(exercise => {
+              return (
+                <Row key={exercise.id}>
+                  <Col className="col-sm-4" />
+                  <Col>
+                    <FormGroup>
+                      <Button
+                        onClick={() => this.removeExercise(exercise)}
+                        color="warning"
+                      >
+                        X
+                      </Button>
+                    </FormGroup>
+                  </Col>
+
+                  <Col>
+                    <FormGroup>
+                      <p>{exercise.name}</p>
+                    </FormGroup>
+                  </Col>
+                </Row>
+              )
+            })}
+          </ul>
 
           <FormGroup className="col-sm-6 offset-sm-3">
-            <Input
-              placeholder="Sets"
-              type="number"
-              step="1"
-              value={this.state.inputSets}
-            />
-          </FormGroup>
-          <FormGroup className="col-sm-6 offset-sm-3">
-            <Input
-              placeholder="Reps"
-              type="number"
-              step="1"
-              value={this.state.inputReps}
-            />
-          </FormGroup>
-
-          <FormGroup className="col-sm-6 offset-sm-3">
-            <Button type="submit" color="primary" block>
-              Add Exercise
+            <Button
+              className="col-sm-6 offset-sm-3"
+              type="submit"
+              color="primary"
+              block
+            >
+              Create Routine
             </Button>
           </FormGroup>
         </Form>
-
-        <ul>
-          {this.state.exercise.map(exercise => {
-            return (
-              <Row key={exercise.id}>
-                <Col className="col-sm-4" />
-                <Col>
-                  <FormGroup>
-                    <Button
-                      onClick={() => this.removeExercise(exercise)}
-                      color="warning"
-                    >
-                      X
-                    </Button>
-                  </FormGroup>
-                </Col>
-
-                <Col>
-                  <FormGroup>
-                    <p>{exercise.name}</p>
-                  </FormGroup>
-                </Col>
-              </Row>
-            )
-          })}
-        </ul>
-
-        <Button
-          className="col-sm-6 offset-sm-3"
-          type="submit"
-          color="primary"
-          block
-        >
-          Create Routine
-        </Button>
       </div>
     )
   }
 }
 
-export default NewRoutine
+const mapState = state => {
+  return {
+    userId: state.user.id
+  }
+}
+
+export default connect(mapState, null)(NewRoutine)
--- server/api/routines.js
+++ server/api/routines.js
@@ -1,11 +1,24 @@
 const router = require('express').Router()
-const {Routine} = require('../db/models')
+const {Routine, User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
     const routines = await Routine.findAll()
     res.json(routines)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const routine = await Routine.create({
+      name: req.body.Title
+    })
+    const user = await User.findOne({where: {id: req.body.user}})
+    await routine.setUser(user)
+    res.json(routine)
   } catch (err) {
     next(err)
   }
